506f6495ceb58d84b204f54c460d5be94ce24ec1
GesaLoof <gesaloof@gmail.com>
2026-05-22 16:06:34 +0200

    updated core settings and urls

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'auth_app'
+    'auth_app',
+    'coderr_app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
