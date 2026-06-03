35b06e9b364d68d7f94d7267949ba133fab0ba72
GesaLoof <gesaloof@gmail.com>
2026-06-03 13:30:56 +0200

    updated choices for status in order model

--- coderr_app/models.py
+++ coderr_app/models.py
@@ -85,8 +85,14 @@ class Order(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     features = models.JSONField(default=list)
     offer_type = models.CharField(max_length=100)
-
-    status = models.CharField(max_length=100, default="pending")
+    STATUS_CHOICES = [
+              ("in_progress", "In Progress"),
+              ("completed", "Completed"),
+              ("cancelled", "Cancelled"),
+          ]
+    status = models.CharField(
+            max_length=20, choices=STATUS_CHOICES, default="in_progress"
+        )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -115,3 +121,4 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review for {self.business_user.user.username} by {self.reviewer.user.username}"
+
